34c6a9c2e18f21e12b76dbbe60a878ee460cd3e2
seungyeon <qorbaxk@nextinnovation.kr>
2023-09-05 17:15:09 +0900

    테일윈드 적용중

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: "#333", // 연한검정색
+          200: "#000", // 진한검정색
+          300: "#E00", // 기본빨강색
+        },
+        Gray: {
+          100: "#BFBFBF", // 보더색, 플레이스 홀더 색
+          200: "#F7F7F7", // 셀렉트 액티브색
+          300: "#737373", // 탭 액티브되지않은 색
+          400: "#D9D9D9", // 사이사이 나누는 색
+        },
+      },
+      fontFamily: {
+        sans: ["Noto Sans KR", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
